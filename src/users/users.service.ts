c0d7c8298629b1606625227db3ded4e938ab7dd6
SyedAnasShah <12.cs.069@gmail.com>
2025-01-19 00:10:42 +0500

    Postgres db setup with typeORM

---
+++ src/users/users.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './users.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(name: string, email: string, password: string): Promise<User> {
+    const user = this.userRepository.create({ name, email, password });
+    return this.userRepository.save(user);
+  }
+
+  async findAllUsers(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+}
+
